ed07303bb4d6911e2c74f131cd75f3b6bae98906
xiongchun <18616786188@qq.com>
2020-05-16 13:09:35 +0800

    Result对象封装

--- pangu-common/src/main/java/com/github/osworks/pangu/common/model/Result.java
+++ pangu-common/src/main/java/com/github/osworks/pangu/common/model/Result.java
@@ -44,14 +44,6 @@ public class Result<T> implements Serializable{
 	 */
 	private T result;
 	
-	public Result(String statusCode, String message, T result){
-		setStatusCode(statusCode).setMessage(message).setResult(result);
-	}
-	
-	public Result(Boolean status, String statusCode, String message, T result){
-		setStatus(status).setStatusCode(statusCode).setMessage(message).setResult(result);
-	}
-	
 	public Result(Boolean status, String statusCode, String message){
 		setStatus(status).setStatusCode(statusCode).setMessage(message);
 	}
@@ -63,17 +55,46 @@ public class Result<T> implements Serializable{
 	 * @return
 	 */
 	public static <T> Result<T> success(T data){
-		return new Result<T>(Constants.Result.SUCCESS, "请求成功", data);
+		return new Result<T>(true,Constants.Result.SUCCESS, "请求成功").setResult(data);
+	}
+	
+	/**
+	 * 请求成功
+	 * @param <T>
+	 * @param data
+	 * @param message
+	 * @return
+	 */
+	public static <T> Result<T> success(T data, String message){
+		return new Result<T>(true, Constants.Result.SUCCESS, message).setResult(data);
+	}
+	
+	/**
+	 * 请求成功
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> Result<T> success(){
+		return new Result<T>(true, Constants.Result.SUCCESS, "请求成功");
 	}
 	
 	/**
 	 * 请求失败
 	 * @param <T>
-	 * @param data
 	 * @return
 	 */
-	public static <T> Result<T> fail(T data){
+	public static <T> Result<T> fail(){
 		return new Result<T>(false, Constants.Result.FAIL, "请求失败");
+	}
+	
+	/**
+	 * 请求失败
+	 * @param <T>
+	 * @param message
+	 * @return
+	 */
+	public static <T> Result<T> fail(String message){
+		return new Result<T>(false, Constants.Result.FAIL, message);
 	}
 	
 }
